dc7a61a9e934c93d3fd04af478a153c6415e6b21
Ludo-code <56892223+Ludo-code@users.noreply.github.com>
2023-07-28 08:21:44 +0200

    Add Powershell script V1 with wordlist

---
+++ Powershell BB1 Auto.ps1
@@ -0,0 +1,39 @@
+$made = 0
+$total = 35
+mode con:cols=80 lines=4
+
+switch ($PSUICulture) 
+{
+    "fr-FR" {$wordlist = Get-Content -Path ".\wordlist\fr-FR.txt"}
+    Default {
+        $wordlist = Get-Content -Path ".\wordlist\en-EN.txt"
+    }
+}
+
+Write-Host "Starting Browser, Please Wait"
+Start-Sleep -Seconds 5
+
+while ($made -lt $total) {
+    $wait = Get-Random -Minimum 3 -Maximum 16
+    $wordlistindex = Get-Random -Minimum 1 -Maximum 501
+    $randword = $wordlist[$wordlistindex]
+    $made++
+
+    Start-Process "microsoft-edge:"
+    Start-Process "microsoft-edge:https://www.bing.com/search?q=$randword"
+
+    Clear-Host
+    $title = "$made Searches Made out of $total - Searching for: $randword"
+    $host.ui.RawUI.WindowTitle = $title
+    Write-Host "$made Searches Made out of $total"
+
+    for ($i = $wait; $i -ge 1; $i--) {
+        [Console]::SetCursorPosition(0, [Console]::CursorTop)
+        Write-Host "$i seconds until next search" -NoNewline
+        Start-Sleep -Seconds 1
+    }
+    
+    Get-Process -Name "msedge" | ForEach-Object { $_.CloseMainWindow() }
+}
+
+Start-Sleep -Seconds 1
